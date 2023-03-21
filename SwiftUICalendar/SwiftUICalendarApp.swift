1c4e1d8c0c08f7c065d39529f073b526a0955879
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2023-03-21 09:45:36 +0100

    Initial Commit

---
+++ SwiftUICalendar/SwiftUICalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalendarApp.swift
+//  SwiftUICalendar
+//
+//  Created by Kaori Persson on 2023-03-21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
